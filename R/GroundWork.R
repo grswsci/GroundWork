3c9e65d5d114f7335e169b0ac87050ed4ec940b4
grswsci <ahmulyy@163.com>
2024-07-17 13:15:40 +0800

    grsw
    grsw

--- R/GroundWork.R
+++ R/GroundWork.R
@@ -97,3 +97,38 @@ write.table(out,
             quote=F)
 return(out)
 }
+getGEO <- function(GEO_ID){
+  options(timeout=1000000)
+  if(nchar(GEO_ID) == 8){
+    url = paste0("http://218.108.182.182:9000/pubmed/DownFile/GEO?fileName=/geo/series/",substr(GEO_ID,1,5),"nnn/",GEO_ID,"/matrix/",GEO_ID,"_series_matrix.txt.gz")
+    destfile <- paste0(GEO_ID,"_series_matrix.txt.gz")
+    download.file(url, destfile, mode = "wb")  
+  }else if(nchar(GEO_ID) == 9){
+    url = paste0("http://218.108.182.182:9000/pubmed/DownFile/GEO?fileName=/geo/series/",substr(GEO_ID,1,6),"nnn/",GEO_ID,"/matrix/",GEO_ID,"_series_matrix.txt.gz")
+    destfile <- paste0(GEO_ID,"_series_matrix.txt.gz")
+    download.file(url, destfile, mode = "wb")  
+  }else if(nchar(GEO_ID) == 7){
+    url = paste0("http://218.108.182.182:9000/pubmed/DownFile/GEO?fileName=/geo/series/",substr(GEO_ID,1,4),"nnn/",GEO_ID,"/matrix/",GEO_ID,"_series_matrix.txt.gz")
+    destfile <- paste0(GEO_ID,"_series_matrix.txt.gz")
+    download.file(url, destfile, mode = "wb")  
+    }
+}
+
+getGEOs <- function(GEO_IDs){
+  options(timeout=1000000)
+  for (GEO_ID in GEO_IDs){
+  if(nchar(GEO_ID) == 8){
+    url = paste0("http://218.108.182.182:9000/pubmed/DownFile/GEO?fileName=/geo/series/",substr(GEO_ID,1,5),"nnn/",GEO_ID,"/matrix/",GEO_ID,"_series_matrix.txt.gz")
+    destfile <- paste0(GEO_ID,"_series_matrix.txt.gz")
+    download.file(url, destfile, mode = "wb")  
+  }else if(nchar(GEO_ID) == 9){
+    url = paste0("http://218.108.182.182:9000/pubmed/DownFile/GEO?fileName=/geo/series/",substr(GEO_ID,1,6),"nnn/",GEO_ID,"/matrix/",GEO_ID,"_series_matrix.txt.gz")
+    destfile <- paste0(GEO_ID,"_series_matrix.txt.gz")
+    download.file(url, destfile, mode = "wb")  
+  }else if(nchar(GEO_ID) == 7){
+    url = paste0("http://218.108.182.182:9000/pubmed/DownFile/GEO?fileName=/geo/series/",substr(GEO_ID,1,4),"nnn/",GEO_ID,"/matrix/",GEO_ID,"_series_matrix.txt.gz")
+    destfile <- paste0(GEO_ID,"_series_matrix.txt.gz")
+    download.file(url, destfile, mode = "wb")  
+  }
+  }
+}
